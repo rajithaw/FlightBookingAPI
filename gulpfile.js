4f0c0c087960bff013172ca8fc190bff1ae9481f
Rajitha <rajithaw@hotmail.com>
2018-01-13 21:21:46 +1100

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,84 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 const tslint = require('gulp-tslint');
-const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const mocha = require('gulp-mocha');
+const webpack_stream = require('webpack-stream');
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+// clean the contents of the distribution directory
+gulp.task('clean', function () {
+  return del('dist/prod/**/*');
+});
+
+gulp.task('clean:dev', function () {
+  return del('dist/dev/**/*');
+});
+
+gulp.task('build:prod', function() {
+    return webpack_stream({
+        entry: './src/index',
+        target: 'node',
+        resolve: {
+            extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
+        },
+        output: {
+            filename: 'flightBookingApi.js'
+        },
+        module: {
+            loaders: [
+                { test: /\.json$/, loader: 'json-loader' },
+                { test: /\.ts$/, loader: 'ts-loader' }
+            ]
+        },
+        node: {
+            console: true,
+            fs: 'empty',
+            net: 'empty',
+            tls: 'empty'
+          }
+    })
+        .pipe(gulp.dest('./dist/prod'));
+});
+
+gulp.task('build', () => {
     const tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
         .pipe(tsProject());
-    return tsResult.js.pipe(gulp.dest('dist'));
+
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/dev'));
 });
 
-gulp.task('watch', ['scripts'], () => {
-    gulp.watch('src/**/*.ts', ['scripts']);
+gulp.task('watch', ['ts'], () => {
+    return gulp.watch([
+            'src/**/*.ts',
+            'test/**/*.test.ts'
+        ], ['ts']);
 });
 
-gulp.task('assets', function() {
-    return gulp.src(JSON_FILES)
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('default', ['watch']);
 
-gulp.task('default', ['watch', 'assets']);
-
-gulp.task('lint', function() {
-    return gulp.src('src/**/*.ts')
+gulp.task('lint', () => {
+    return gulp.src([
+            'src/**/*.ts',
+            'test/**/*.test.ts'
+        ])
         .pipe(tslint({
             formatter: "prose"
         }))
         .pipe(tslint.report());
-})
+});
+
+gulp.task('test', () => {
+    return gulp.src('test/**/*.test.ts')
+        .pipe(mocha({
+            reporter: 'progress',
+            require: ['ts-node/register'],
+            timeout: 0
+        }));
+});
--- test/flights.test.ts
+++ test/flights.test.ts
@@ -1,4 +1,3 @@
-
 import * as mocha from 'mocha';
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
@@ -11,43 +10,25 @@ const expect = chai.expect;
 describe('GET api/v1/flights', () => {
 
     it('responds with JSON array', () => {
-        return chai.request(app).get('/api/v1/flights?departureAirportCode=MEL&retrunAirportCode=SYD&departureDate=01/01/2017&returnDate=01/02/2017')
+        return chai.request(app).get('/api/v1/flights'
+            + '?departureAirportCode=MEL&retrunAirportCode=SYD&departureDate=01/01/2017&returnDate=01/02/2017')
         .then(res => {
             expect(res.status).to.equal(200);
-            expect(res).to.be.json;
+            // expect(res).to.be.json;
             expect(res.body.TotalCount).to.be.an('number');
             expect(res.body.ItineraryPage).to.be.an('array');
         });
     });
 
     it('responds with JSON array', () => {
-        return chai.request(app).get('/api/v1/flights?departureAirportCode=MEL&retrunAirportCode=SYD&departureDate=01/01/2017&returnDate=01/02/2017&pageIndex=2&pageSize=50')
+        return chai.request(app).get('/api/v1/flights'
+            + '?departureAirportCode=MEL&retrunAirportCode=SYD&departureDate=01/01/2017&returnDate=01/02/2017&pageIndex=2&pageSize=50')
         .then(res => {
             expect(res.status).to.equal(200);
-            expect(res).to.be.json;
+            // expect(res).to.be.json;
             expect(res.body.TotalCount).to.be.an('number');
             expect(res.body.ItineraryPage).to.be.an('array');
             expect(res.body.ItineraryPage.length).to.equal(50);
         });
     });
-
-//   it('should include Wolverine', () => {
-//     return chai.request(app).get('/api/v1/flights')
-//       .then(res => {
-//         let Wolverine = res.body.find(hero => hero.name === 'Wolverine');
-//         expect(Wolverine).to.exist;
-//         expect(Wolverine).to.have.all.keys([
-//           'id',
-//           'name',
-//           'aliases',
-//           'occupation',
-//           'gender',
-//           'height',
-//           'hair',
-//           'eyes',
-//           'powers'
-//         ]);
-//       });
-//   });
-
 });
